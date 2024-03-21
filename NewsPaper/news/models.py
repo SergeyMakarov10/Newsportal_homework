9177419d127f3d8fd3abc210292d03cef365e898
Sergey Makarov <134254527+SergeyMakarov10@users.noreply.github.com>
2024-03-21 22:35:15 +0400

    Update models.py
    removed extra blank line

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -47,7 +47,6 @@ class Post (models.Model):
         return text
 
 
-
 class PostCategory (models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
